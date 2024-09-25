97f9fd12dc7d267049b1b0bfae20cf03be87406f
123-Mathias <u202216282@upc.edu.pe>
2024-09-25 11:46:45 -0500

    feat: add bottom navigation bar
    Implemented BottomBarNavigation for easier navigation between main screens.

---
+++ app/src/main/java/com/techzo/cambiazo/presentation/navigate/BottomBarNavigation.kt
@@ -0,0 +1,45 @@
+package com.techzo.cambiazo.presentation.navigate
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BottomBarNavigation(items: List<ItemsScreens>) {
+    BottomAppBar(
+        modifier = Modifier
+            .border(width = 1.dp, color = Color.Gray),
+        containerColor = Color.White,
+    ) {
+        NavigationBar(
+            modifier = Modifier.background(Color.White),
+            containerColor = Color.White,
+        ) {
+            items.forEach { screen ->
+                NavigationBarItem(
+                    selected = false,
+                    onClick = { screen.navigate() },
+                    icon = {
+                        Icon(
+                            imageVector = screen.icon,
+                            contentDescription = screen.title,
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .size(35.dp)
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
