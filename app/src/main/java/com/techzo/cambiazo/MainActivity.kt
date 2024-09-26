19ff884ea03f329d6d69257bc77ff7879e0ac42e
IanHD04 <103000300+IanHD04@users.noreply.github.com>
2024-09-25 22:05:16 -0500

    feat: make status bar transparent
    Set the status bar to be transparent on mobile devices, creating a more immersive and modern user interface.

--- app/src/main/java/com/techzo/cambiazo/MainActivity.kt
+++ app/src/main/java/com/techzo/cambiazo/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.techzo.cambiazo.common.Constants
 import com.techzo.cambiazo.data.remote.AuthService
 import com.techzo.cambiazo.data.repository.AuthRepository
 import com.techzo.cambiazo.presentation.login.SignInViewModel
 import com.techzo.cambiazo.presentation.navigate.NavScreen
 import com.techzo.cambiazo.ui.theme.CambiazoTheme
+import com.techzo.cambiazo.ui.theme.ScreenBackground
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -25,10 +29,14 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContent {
             CambiazoTheme {
-                    NavScreen(viewModel)
+                val backgroundColor = ScreenBackground
+                window.statusBarColor = backgroundColor.toArgb()
+                WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+                NavScreen(viewModel)
             }
         }
     }
