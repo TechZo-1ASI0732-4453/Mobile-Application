d6a51c25f8b77c9299634560c918cb9a335e2780
123-Mathias <u202216282@upc.edu.pe>
2024-10-04 10:20:03 -0500

    feat: created input for money
    Added an input component specifically for handling monetary values.

--- app/src/main/java/com/techzo/cambiazo/common/components/CustomInputs.kt
+++ app/src/main/java/com/techzo/cambiazo/common/components/CustomInputs.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -13,6 +14,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -33,5 +36,33 @@ fun FieldTextApp(valueText:String,text:String,onValueChange: (String) -> Unit){
             .clip(RoundedCornerShape(10.dp))
             .border(1.dp, Color.Gray, RoundedCornerShape(10.dp)),
         shape = RoundedCornerShape(10.dp),
+    )
+}
+
+@Composable
+fun MoneyFieldApp(valueText:String,text:String, modifier: Modifier = Modifier,onValueChange: (String) -> Unit){
+
+    OutlinedTextField(
+        value = valueText,
+        placeholder = {
+            Text(text, color = Color.Gray,
+                style= MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = FontFamily.SansSerif))
+        },
+        onValueChange = { newValue ->
+            if (newValue.isEmpty() || newValue.matches(Regex("^\\d*(\\.\\d{0,2})?\$"))) {
+                onValueChange(newValue)
+            }},
+
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+            .border(1.dp, Color.Gray, RoundedCornerShape(12.dp)),
+        shape = RoundedCornerShape(12.dp),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
+        )
     )
 }
