4a534efbcfa0b2dff1a155eb2b9671a568f5fc0f
IanHD04 <134337719+Jemisas@users.noreply.github.com>
2024-10-17 21:50:22 -0500

    feat: added review dto, service and entity
    Added Review DTO, Service, and Entity components for handling review functionality in the project.

---
+++ app/src/main/java/com/techzo/cambiazo/data/remote/reviews/ReviewService.kt
@@ -0,0 +1,23 @@
+package com.techzo.cambiazo.data.remote.reviews
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ReviewService {
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @GET("reviews/user-receptor/{userId}")
+    suspend fun getReviewsByUserId(@Path("userId") userId: Int): Response<List<ReviewDto>>
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @POST("reviews")
+    suspend fun addReview(@Body reviewDto: ReviewDto): Response<ReviewDto>
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @DELETE("reviews/delete/{reviewId}")
+    suspend fun deleteReview(@Path("reviewId") reviewId: Int): Response<ReviewDto>
+}
