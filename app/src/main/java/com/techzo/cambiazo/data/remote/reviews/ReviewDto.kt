998ec5e2929907e6630c4c723376d110cc86d6ba
IanHD04 <103000300+IanHD04@users.noreply.github.com>
2024-10-19 14:20:59 -0500

    feat: create reviewAverageUser entity and reviewAverageUserDTO
    Added reviewAverageUser entity and reviewAverageUserDTO to display the user's average star rating and review count on their profile.

--- app/src/main/java/com/techzo/cambiazo/data/remote/reviews/ReviewDto.kt
+++ app/src/main/java/com/techzo/cambiazo/data/remote/reviews/ReviewDto.kt
@@ -1,6 +1,7 @@
 package com.techzo.cambiazo.data.remote.reviews
 
 import com.techzo.cambiazo.domain.Review
+import com.techzo.cambiazo.domain.ReviewAverageUser
 
 data class ReviewDto(
     val message: String,
@@ -19,5 +20,17 @@ fun ReviewDto.toReview(): Review {
         exchangeId = exchangeId,
         userAuthorId = userAuthorId,
         userReceptorId = userReceptorId
+    )
+}
+
+data class ReviewAverageUserDto(
+    val averageRating: Double,
+    val countReviews: Int,
+)
+
+fun ReviewAverageUserDto.toReviewAverageUser(): ReviewAverageUser {
+    return ReviewAverageUser(
+        averageRating = averageRating,
+        countReviews = countReviews
     )
 }
