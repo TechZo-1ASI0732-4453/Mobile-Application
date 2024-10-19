707129fd0cbccc5781417f9b2eb1877724001f62
IanHD04 <103000300+IanHD04@users.noreply.github.com>
2024-10-19 14:19:31 -0500

    feat: create profile screen component with background, card, and centered user image
    Created a profile screen component with a background, card layout, and a user image centered between the header and card.

--- app/src/main/java/com/techzo/cambiazo/common/components/BaseBackground.kt
+++ app/src/main/java/com/techzo/cambiazo/common/components/BaseBackground.kt
@@ -5,16 +5,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun CardApp(padding: PaddingValues, content: @Composable () -> Unit = {}) {
@@ -44,9 +53,9 @@ fun CardApp(padding: PaddingValues, content: @Composable () -> Unit = {}) {
 fun MainScaffoldApp(paddingCard: PaddingValues,
                     bottomBar: @Composable () -> Unit = {},
                     contentsHeader: @Composable () -> Unit = {},
+                    profileImage: (@Composable () -> Unit)? = null,
                     content: @Composable () -> Unit = {},
-
-                    ) {
+) {
     Scaffold(
         bottomBar = bottomBar,
     ) { paddingValues->
@@ -59,9 +68,35 @@ fun MainScaffoldApp(paddingCard: PaddingValues,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             contentsHeader()
+
+            profileImage?.let {
+                it()
+            }
+
             CardApp(paddingCard){
                 content()
             }
         }
+    }
+}
+
+
+@Composable
+fun ProfileImage(url:String, shape: Shape, size: Dp){
+    Surface(
+        modifier= Modifier
+            .size(size)
+            .zIndex(1f)
+            .offset(y = (size/2))
+            .background(Color.Transparent)
+            .clip(RoundedCornerShape(20.dp)),
+        shape = shape,
+    ) {
+        GlideImage(
+            imageModel = { url },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(20.dp)
+        )
     }
 }
