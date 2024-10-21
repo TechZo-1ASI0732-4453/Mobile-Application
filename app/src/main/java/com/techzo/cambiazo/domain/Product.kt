9d8ada3d6ee97f5a36451f7dc1da2865e2e4d6c0
123-Mathias <u202216282@upc.edu.pe>
2024-10-21 12:36:23 -0500

    fix: update Product model
    Reorganized Product model to align with ProductDto.

--- app/src/main/java/com/techzo/cambiazo/domain/Product.kt
+++ app/src/main/java/com/techzo/cambiazo/domain/Product.kt
@@ -1,5 +1,8 @@
 package com.techzo.cambiazo.domain
 
+import com.techzo.cambiazo.data.remote.products.Location
+import com.techzo.cambiazo.data.remote.products.ProductCategory
+
 data class Product(
     val id: Int,
     val name: String,
@@ -9,10 +12,7 @@ data class Product(
     val image: String,
     val boost: Boolean,
     val available: Boolean,
-    val productCategoryId: Int,
+    val productCategory: ProductCategory,
     val userId: Int,
-    val districtId: Int,
-    var district: District? = null,
-    var country: Country? = null,
-    var department: Department? = null,
+    val location: Location,
 )
