4b3738dc8d824516ebea70c073d8590014296aa8
IanHD04 <103000300+IanHD04@users.noreply.github.com>
2024-10-21 01:31:09 -0500

    feat: add getReviewsByUserId function in ReviewRepository
    Created getReviewsByUserId function to fetch a list of reviews for a user by their ID, handling API responses and errors with appropriate resource management.

--- app/src/main/java/com/techzo/cambiazo/data/repository/ReviewRepository.kt
+++ app/src/main/java/com/techzo/cambiazo/data/repository/ReviewRepository.kt
@@ -2,7 +2,9 @@ package com.techzo.cambiazo.data.repository
 
 import com.techzo.cambiazo.common.Resource
 import com.techzo.cambiazo.data.remote.reviews.ReviewService
+import com.techzo.cambiazo.data.remote.reviews.toReview
 import com.techzo.cambiazo.data.remote.reviews.toReviewAverageUser
+import com.techzo.cambiazo.domain.Review
 import com.techzo.cambiazo.domain.ReviewAverageUser
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -21,6 +23,23 @@ class ReviewRepository(private val reviewService: ReviewService) {
             return@withContext Resource.Error(e.message ?: "Ocurrió un error")
         }
     }
+
+    suspend fun getReviewsByUserId(userId: Int): Resource<List<Review>> =
+        withContext(Dispatchers.IO) {
+            try {
+                val response = reviewService.getReviewsByUserId(userId)
+                if (response.isSuccessful) {
+                    response.body()?.let { reviewsDto ->
+                        val reviews = reviewsDto.map { it.toReview() }
+                        return@withContext Resource.Success(data = reviews)
+                    }
+                    return@withContext Resource.Error("No reviews found")
+                }
+                return@withContext Resource.Error(response.message())
+            } catch (e: Exception) {
+                return@withContext Resource.Error(e.message ?: "An error occurred")
+            }
+        }
 
 }
 
