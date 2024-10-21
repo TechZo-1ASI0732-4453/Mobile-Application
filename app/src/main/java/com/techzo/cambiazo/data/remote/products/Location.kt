1733e49dd5fa388eabe7d446a8ee594254d14313
123-Mathias <u202216282@upc.edu.pe>
2024-10-21 12:32:45 -0500

    fix: updated ProductDto structure
    Reorganized ProductDto to align with new backend structure.

---
+++ app/src/main/java/com/techzo/cambiazo/data/remote/products/Location.kt
@@ -0,0 +1,10 @@
+package com.techzo.cambiazo.data.remote.products
+
+data class Location(
+    val countryId: Int,
+    val countryName: String,
+    val departmentId: Int,
+    val departmentName: String,
+    val districtId: Int,
+    val districtName: String
+)
