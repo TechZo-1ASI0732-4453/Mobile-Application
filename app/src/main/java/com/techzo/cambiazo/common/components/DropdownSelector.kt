0a5125839b53400bee68b37749e5ed7fd01f4913
123-Mathias <u202216282@upc.edu.pe>
2024-10-22 04:04:23 -0500

    feat: created custom DropdownSelect with error validation
    Developed custom DropdownSelect with error handling.

--- app/src/main/java/com/techzo/cambiazo/common/components/DropdownSelector.kt
+++ app/src/main/java/com/techzo/cambiazo/common/components/DropdownSelector.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -33,11 +34,15 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toSize
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
+import androidx.compose.ui.zIndex
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
 
 @Composable
 fun  <T> DropdownList(
@@ -143,3 +148,130 @@ fun  <T> DropdownList(
         }
     }
 }
+
+
+@Composable
+fun  <T> CustomDropDownSelect(
+    selectedOption: T?,
+    label: String,
+    itemList: List<T>,
+    isError: Boolean = false,
+    messageError: String = "Invalid field",
+    onItemClick: (T?) -> Unit,
+    itemToString: (T) -> String
+) {
+    var textFilledSize by remember { mutableStateOf(Size.Zero) }
+    var showDropdown by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CustomInput(
+            value = selectedOption?.let(itemToString) ?: "",
+            placeHolder = label,
+            type = "Text",
+            trailingIcon = {
+                IconButton( onClick = { showDropdown = !showDropdown }) {
+                    Icon(
+                        imageVector = if (showDropdown) Icons.Filled.ArrowDropUp else Icons.Filled.ArrowDropDown,
+                        contentDescription = null,
+                        tint = Color.Black
+                    )
+                }
+            },
+            modifier = Modifier.onGloballyPositioned { coordinates ->
+                textFilledSize = coordinates.size.toSize()
+            },
+            readOnly = true,
+            isError = isError,
+            messageError = null,
+            onValueChange = { },
+        )
+        Box {
+            if (isError){
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = messageError,
+                    color = Color.Red,
+                    textAlign = TextAlign.Start
+                )
+            }
+            if (showDropdown) {
+                CustomDropDownMenu(
+                    hideMenu = { showDropdown = false },
+                    itemList = itemList,
+                    onItemClick = { onItemClick(it) },
+                    itemToString = itemToString,
+                    width = with(LocalDensity.current) { textFilledSize.width.toDp() })
+            }
+        }
+
+    }
+}
+
+@Composable
+fun <T> CustomDropDownMenu(
+    hideMenu:() -> Unit,
+    itemList:List<T>,
+    onItemClick: (T?) -> Unit,
+    width: Dp,
+    itemToString: (T) -> String)
+{
+    val scrollState = rememberScrollState()
+
+            Popup(
+                alignment = Alignment.TopCenter,
+                properties = PopupProperties(excludeFromSystemGesture = true),
+                onDismissRequest = {  },
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(width)
+                        .heightIn(max = 170.dp)
+                        .shadow(
+                            elevation = 5.dp,
+                            shape = RoundedCornerShape(5.dp),
+                            clip = true
+                        )
+                        .verticalScroll(state = scrollState),
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.White)
+                            .clickable {
+                                hideMenu()
+                                onItemClick(null)
+                            },
+                    ) {
+                        Text(
+                            text = "Ninguno",
+                            color = Color.Gray,
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp)
+                        )
+                    }
+                    itemList.forEach { item ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.White)
+                                .clickable {
+                                    hideMenu()
+                                    onItemClick(item)
+                                },
+                        ) {
+                            Text(
+                                text = item.let(itemToString),
+                                color = Color.Gray,
+                                fontSize = 14.sp,
+                                modifier = Modifier.padding(vertical = 5.dp, horizontal = 10.dp)
+                            )
+                        }
+                    }
+                }
+            }
+}
+
