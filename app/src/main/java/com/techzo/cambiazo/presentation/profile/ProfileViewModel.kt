7e185b4f589e8e1f94c8fb065081ede6c437aa4a
IanHD04 <103000300+IanHD04@users.noreply.github.com>
2024-10-22 13:40:23 -0500

    feat: activate ProfileViewModel for data refresh after updates
    Updated ProfileViewModel to ensure user data is refreshed after profile changes. Added refreshUserData method to reload user information from the repository,

--- app/src/main/java/com/techzo/cambiazo/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/techzo/cambiazo/presentation/profile/ProfileViewModel.kt
@@ -8,14 +8,17 @@ import com.techzo.cambiazo.common.Constants
 import com.techzo.cambiazo.common.Resource
 import com.techzo.cambiazo.common.UIState
 import com.techzo.cambiazo.data.repository.ReviewRepository
+import com.techzo.cambiazo.data.repository.UserRepository
 import com.techzo.cambiazo.domain.ReviewAverageUser
+import com.techzo.cambiazo.domain.User
+import com.techzo.cambiazo.domain.UserSignIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
-class ProfileViewModel@Inject constructor(private val reviewRepository: ReviewRepository) : ViewModel() {
+class ProfileViewModel@Inject constructor(private val reviewRepository: ReviewRepository, private val userRepository: UserRepository) : ViewModel() {
 
     private val _averageRating =mutableStateOf<Double?>(null)
     val averageRating: State<Double?> get() = _averageRating
@@ -27,9 +30,45 @@ class ProfileViewModel@Inject constructor(private val reviewRepository: ReviewRe
     val state: State<UIState<ReviewAverageUser>> get() = _state
 
 
+    private val _userState = mutableStateOf(UIState<User>())
+    val userState: State<UIState<User>> get() = _userState
+
+
+    private val _user = mutableStateOf<User?>(null)
+    val user: State<User?> get() = _user
+
+    private fun loadUserData() {
+        viewModelScope.launch {
+            val user = userRepository.getUserById(Constants.user!!.id)
+            if (user is Resource.Success) {
+                _user.value = user.data
+            }
+        }
+    }
+
+    fun refreshUserData() {
+        loadUserData()
+    }
+
     init {
         getReviewData()
+        getUserData()
     }
+
+
+
+    private fun getUserData() {
+        _userState.value = UIState(isLoading = true)
+        viewModelScope.launch {
+            val user = userRepository.getUserById(Constants.user!!.id)
+            if (user is Resource.Success) {
+                _userState.value = UIState(data = user.data)
+            } else {
+                _userState.value = UIState(message = user.message ?: "Error")
+            }
+        }
+    }
+
 
     fun getReviewData() {
         _state.value = UIState(isLoading = true)
