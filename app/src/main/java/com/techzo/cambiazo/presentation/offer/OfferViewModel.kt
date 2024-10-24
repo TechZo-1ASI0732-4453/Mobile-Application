f324809fcfff8282ba210ad26adcc1a547e46cf7
Jemisas <134337719+Jemisas@users.noreply.github.com>
2024-10-24 08:47:33 -0500

    feat: added 'ConfirmationOfferScreen'
    Added 'ConfirmationOfferScreen' to display offer confirmation details.

--- app/src/main/java/com/techzo/cambiazo/presentation/offer/OfferViewModel.kt
+++ app/src/main/java/com/techzo/cambiazo/presentation/offer/OfferViewModel.kt
@@ -14,7 +14,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OfferViewModel @Inject constructor(
-    private val repositoryExchange: ExchangeRepository // No más repositoryProduct aquí
+    private val repositoryExchange: ExchangeRepository
 ) : ViewModel() {
 
     private val _selectedOfferedProduct = mutableStateOf<Product?>(null)
