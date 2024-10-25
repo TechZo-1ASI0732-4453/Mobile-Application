a24dc6846e49e8b7b005e29e12248341e53667ba
123-Mathias <u202216282@upc.edu.pe>
2024-10-25 13:54:46 -0500

    style: adjusted navbar icon sizes for better layout
    Updated BottomNavBar design, adjusting icon sizes for better visual alignment.

--- app/src/main/java/com/techzo/cambiazo/presentation/navigate/BottomBarNavigation.kt
+++ app/src/main/java/com/techzo/cambiazo/presentation/navigate/BottomBarNavigation.kt
@@ -1,22 +1,30 @@
 package com.techzo.cambiazo.presentation.navigate
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -25,41 +33,49 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun BottomBarNavigation(items: List<ItemsScreens>,currentScreen: String) {
 
+
         NavigationBar(
-            modifier = Modifier.border(width = 1.dp,color = Color.Gray).height(100.dp),
+            modifier = Modifier
+                .height(75.dp)
+                .shadow(elevation = 15.dp,
+                    spotColor = Color.Black,
+                    ambientColor = Color.Black,
+                    clip = true
+                ),
             containerColor = Color.White,
 
         ) {
 
             items.forEach { screen ->
                 val isCurrentScreen = currentScreen == screen.route
-                val color = if (isCurrentScreen) Color(0xFFFFD146) else Color.Black
+                val color = if (isCurrentScreen) Color(0xFFFFD146) else Color.Gray.copy(alpha = 1f)
                 NavigationBarItem(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(top = 8.dp)
                         .drawBehind {
-                        drawRect(
-                            color = if (isCurrentScreen) Color(0xFFFFD146) else Color.Transparent,
-                            topLeft = Offset(0f, -20f),
-                            size = Size(size.width, 10f),
-                        )
-                    },
+                            drawRect(
+                                color = if (isCurrentScreen) Color(0xFFFFD146) else Color.Transparent,
+                                topLeft = Offset(0f, -22f),
+                                size = Size(size.width, 10f),
+                            )
+                        },
                     alwaysShowLabel = true,
                     selected = false,
                     onClick = { screen.navigate() },
                     icon = {
                         Icon(
-                            imageVector =  screen.icon ,
+                            imageVector = screen.icon,
                             contentDescription = screen.title,
                             tint = color,
-                            modifier = Modifier.size(35.dp)
+                            modifier = Modifier.size(27.dp)
                         )
                     },
                     label = {
                         Text(
                             modifier = Modifier.fillMaxWidth(),
                             text = screen.title,
+                            fontSize = 10.sp,
                             maxLines = 1,
                             color = color,
                             letterSpacing = 0.001.sp,
@@ -70,4 +86,5 @@ fun BottomBarNavigation(items: List<ItemsScreens>,currentScreen: String) {
                 )
             }
         }
+
 }
