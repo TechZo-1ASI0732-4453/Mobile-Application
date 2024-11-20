86cf3f7b9a4afba3253bcbd6b460cac442c62442
Joseph Huamani | Nerk <u20221a133@upc.edu.pe>
2024-11-20 16:43:51 -0500

    feat: added method existe reviews by user and exchange IDs
    I've added a new method in the ReviewService to retrieve a boolean if reviews based on a user's ID and an exchange ID exists, returning an ExistReview response.

--- app/src/main/java/com/techzo/cambiazo/data/remote/reviews/ReviewService.kt
+++ app/src/main/java/com/techzo/cambiazo/data/remote/reviews/ReviewService.kt
@@ -22,6 +22,11 @@ interface ReviewService {
     @POST("reviews")
     suspend fun addReview(@Body reviewDto: ReviewRequestDto): Response<ReviewDto>
 
+
+    @Headers("Content-Type: application/json", "Accept: application/json")
+    @GET("reviews/user-author/{userId}/exchange/{exchangeId}")
+    suspend fun getReviewsByUserId(@Path("userId") userId: Int, @Path("exchangeId") exchangeId: Int): Response<ExistReview>
+
     @Headers("Content-Type: application/json", "Accept: application/json")
     @DELETE("reviews/delete/{reviewId}")
     suspend fun deleteReview(@Path("reviewId") reviewId: Int): Response<ReviewDto>
