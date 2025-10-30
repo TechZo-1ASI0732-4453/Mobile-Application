13f13d8637c4127a0cce0389815140b52001927a
Mathias Mendoza Carrion <u202216282@upc.edu.pe>
2025-10-29 19:42:21 -0500

    fix: Updated BASE_URL to new API v2 endpoint
    Changed the BASE_URL constant to point to the /api/v2/

--- app/src/main/java/com/techzo/cambiazo/common/Constants.kt
+++ app/src/main/java/com/techzo/cambiazo/common/Constants.kt
@@ -5,7 +5,7 @@ import com.techzo.cambiazo.domain.Subscription
 import com.techzo.cambiazo.domain.UserSignIn
 
 object Constants {
-    const val BASE_URL = "https://cambiazo-techzo-gzdtcfcca4fxeaec.chilecentral-01.azurewebsites.net/"
+    const val BASE_URL = "https://cambiazo-techzo-gzdtcfcca4fxeaec.chilecentral-01.azurewebsites.net/api/v2/"
     const val DEFAULT_PROFILE_PICTURE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6lqpQj3oAmc1gtyM78oJCbTaDrD7Fj9NRlceOPDZiHA&s"
     const val DEFAULT_ROLE = "ROLE_USER"
     var token: String? = null
